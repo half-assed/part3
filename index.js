3b6baae6d1fccb442d1ee7611ed2f4e05fa6d54d
My Name <oscar.vonnandelstadh@centria.fi>
2021-02-23 17:22:16 +0200

    3.16: Phonebook database, step4

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('build'))      // frontend build
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data')) */
 
 // 3.5
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   console.log(">>> POST - /api/persons")
   const newPerson = new Person({
       name: request.body.name,
@@ -25,23 +25,19 @@ app.post('/api/persons', (request, response) => {
       newPerson.save().then(result => {
         console.log("Save: ", result)
         response.json(result)
-      }).catch(error => {
-        console.log(">>>ERROR<<< Saving failed: \n", error)
-      })
+      }).catch(error => next(error))
   }
   else
       response.status(400).send( { error: 'name must be unique' } )
 })
 
 // 3.13
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   console.log(">>> GET - /api/persons")
   Person.find({}).then(result => {
     response.json(result)
   })
-  .catch(error => {
-    console.log(">>> ERROR <<<", error)
-  })
+  .catch(error => next(error))
 
 })
 
@@ -51,12 +47,19 @@ app.get('/info', (request, response) => {
 })
 
 // 3.3
-app.get('/api/persons/:id', (request, response) => {
-    const person = persons.find(person => person.id === Number(request.params.id))
+app.get('/api/persons/:id', (request, response, next) => {
+  console.log("Route: /api/persons/:id")
+
+  Person.find({}).then(result => {
+    const person = result.find(person => person._id.equals(request.params.id))
     if (person)
-        response.json(person)
+      response.json(person)
     else
-        response.status(404).end()
+      response.status(404).end()
+  })
+  .catch(error => next(error))
+
+
 })
 
 // 3.4
@@ -64,8 +67,18 @@ app.delete('/api/persons/:id', (request, response, next) => {
   console.log(">>> DELETE - /api/persons")
   person.findByIdAndDelete(request.params.id).then(result => {
     response.status(204).end()
-  }).catch(error => {console.log(">>>ERROR<<<", error)})
+  }).catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.log("An error occured:")
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
